a95f5ef386cba9f817c15f0d3ac6ea89ffb2c9d9
Jelita <jelitasyahwa@gmail.com>
2026-06-14 08:04:51 +0700

    update tembereng

--- SistemGeometriElips/src/Bangun2Dimensi/Tembereng.java
+++ SistemGeometriElips/src/Bangun2Dimensi/Tembereng.java
@@ -1,63 +1,45 @@
 package Bangun2Dimensi;
 
 /**
- * Tembereng: daerah elips dipotong tali busur.
- * Konsep: juring elips - segitiga pusat.
+ * Tembereng: bagian elips (juring - segitiga).
+ * Konsep: fraksi elips dikurangi segitiga pusat.
  */
 public class Tembereng extends Elips implements Runnable {
 
-    // ====== ATRIBUT PUBLIC ======
+    // ===== ATRIBUT PUBLIC =====
     public double sudut;
 
-    // ====== CONSTRUCTOR ======
-    public Tembereng(
-            double semiMayor,
-            double semiMinor,
-            double sudut,
-            int jumlahData
-    ) throws Exception {
+    // ===== CONSTRUCTOR =====
+    public Tembereng(double semiMayor, double semiMinor, double sudut, int jumlahData) throws Exception {
 
         super(semiMayor, semiMinor, jumlahData);
-
-        if (sudut <= 0 || sudut >= 360) {
-            throw new IllegalArgumentException(
-                "[Tembereng] Sudut harus 0 - 360"
-            );
-        }
-
-        this.sudut = sudut;
-        this.namaThread = "Thread-Tembereng";
-
-        System.out.println(
-            "[LOG][Bangun2Dimensi.Tembereng] Constructor dipanggil: a="
-            + semiMayor + ", b=" + semiMinor + ", sudut=" + sudut
-        );
-    }
-
-    // ====== OVERRIDE LUAS ======
-    @Override
-    public double hitungLuas() {
 
         if (sudut <= 0 || sudut >= 360) {
             throw new IllegalArgumentException("[Tembereng] Sudut tidak valid");
         }
 
-        // luas elips penuh dari parent
+        this.sudut = sudut;
+        this.namaThread = "Thread-Tembereng";
+
+        System.out.println("[LOG][Tembereng] Constructor dipanggil");
+    }
+
+    // ===== OVERRIDE LUAS (STATE BASED) =====
+    @Override
+    public double hitungLuas() {
+
         double luasElips = super.hitungLuas();
 
-        // juring elips
         double juring = (sudut / 360.0) * luasElips;
 
-        // segitiga pusat elips
         double rad = Math.toRadians(sudut);
         double segitiga = 0.5 * semiMayor * semiMinor * Math.sin(rad);
 
         hasilLuas = juring - segitiga;
-
         return hasilLuas;
     }
 
-    // ====== OVERLOADING (sesuai interface) ======
+    // ===== OVERLOADING WAJIB INTERFACE =====
     @Override
     public double hitungLuas(double a, double b) {
 
@@ -68,7 +50,7 @@ public class Tembereng extends Elips implements Runnable {
         return super.hitungLuas(a, b);
     }
 
-    // ====== OVERLOADING KHUSUS (utility optional) ======
+    // ===== OVERLOADING TAMBAHAN (LEVEL JURING STYLE) =====
     public double hitungLuas(double a, double b, double sudutDeg) {
 
         if (a <= 0 || b <= 0 || sudutDeg <= 0 || sudutDeg >= 360) {
@@ -76,6 +58,7 @@ public class Tembereng extends Elips implements Runnable {
         }
 
         double luasElips = Math.PI * a * b;
+
         double juring = (sudutDeg / 360.0) * luasElips;
 
         double rad = Math.toRadians(sudutDeg);
@@ -84,31 +67,10 @@ public class Tembereng extends Elips implements Runnable {
         return juring - segitiga;
     }
 
-    // ====== KELILING ======
-        @Override
+    // ===== KELILING =====
+    @Override
     public double hitungKeliling() {
-
-        if (sudut <= 0 || sudut >= 360) {
-            throw new IllegalArgumentException(
-                "[Tembereng] Sudut tidak valid"
-            );
-        }
-
-        // keliling elips penuh dari parent
-        double kelilingElips = super.hitungKeliling();
-
-        // panjang busur
-        double busur =
-                (sudut / 360.0) * kelilingElips;
-
-        // konversi sudut ke radian
-        double rad = Math.toRadians(sudut);
-
-        // pendekatan tali busur
-        double taliBusur =
-                2 * semiMayor * Math.sin(rad / 2);
-
-        return busur + taliBusur;
+        return super.hitungKeliling();
     }
 
     @Override
@@ -116,7 +78,7 @@ public class Tembereng extends Elips implements Runnable {
         return super.hitungKeliling(a, b);
     }
 
-    // ====== TAMPIL INFO ======
+    // ===== TAMPIL INFO =====
     public void tampilInfo() {
 
         System.out.println("=== TEMBERENG ELIPS ===");
@@ -132,7 +94,7 @@ public class Tembereng extends Elips implements Runnable {
         }
     }
 
-    // ====== THREAD ======
+    // ===== THREAD (SAMA STYLE JURING & CINCIN) =====
     @Override
     public void run() {
 
@@ -169,7 +131,7 @@ public class Tembereng extends Elips implements Runnable {
         } catch (Exception e) {
 
             statusThread = "ERROR";
-            System.out.println("[Bangun2Dimensi.Tembereng] " + e.getMessage());
+            System.out.println("[Tembereng] " + e.getMessage());
         }
     }
 }
